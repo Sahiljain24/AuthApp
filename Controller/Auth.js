4090cd10c35104130e1ccc0365a09e3f541ec51d
Sahiljain24 <24sahilj@gmail.com>
2024-05-23 22:17:59 +0530

    update

--- Controller/Auth.js
+++ Controller/Auth.js
@@ -1,7 +1,13 @@
+const { default: mongoose } = require("mongoose");
 const user = require("../Models/User");
 
 exports.login =(req,res)=>{
     const {email ,password} =req.body;
 
-    
+    const isUser = mongoose.findOne({email});
+    if(!isUser){
+        res.status(404){
+            
+        }
+    }
 }
